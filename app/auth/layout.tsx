439100c39b527ea0235d1a0c14437cdde855d898
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-06-25 18:53:28 -0300

    feat: create auth layout and login page

---
+++ app/auth/layout.tsx
@@ -0,0 +1,11 @@
+type AuthLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      {children}
+    </div>
+  );
+}
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "@/components/auth/form-login";
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
