5f952b4923bb27af6d29f6bf3b662e9bc31af5c0
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-07-01 17:05:39 -0300

    feat: create register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,13 @@ export const loginSchema = z.object({
     message: "Password is required",
   }),
 });
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
